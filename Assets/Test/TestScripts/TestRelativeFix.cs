eca10f7212c85aaaa5b2124e964a4da2f178fc4d
zhangqi <379497489@qq.com>
2018-10-17 19:53:34 +0800

    adjust the test script and scene of relative fix

--- Assets/Test/TestScripts/TestRelativeFix.cs
+++ Assets/Test/TestScripts/TestRelativeFix.cs
@@ -1,8 +1,22 @@
 ﻿using UnityEngine;
-using CrazyBox.Tools;
 using CrazyBox.Systems;
+using UnityEngine.EventSystems;
 
 public class TestRelativeFix : MonoBehaviour {
+
+    void Start()
+    {
+        DragHandler.Get(gameObject).OnDragAction = OnDrag;
+    }
+
+    public void OnDrag(PointerEventData ped)
+    {
+        Vector3 pos = transform.localPosition;
+        Vector3 scale = transform.lossyScale;
+        pos.Set(pos.x + ped.delta.x * (1 / scale.x),
+            pos.y + ped.delta.y * (1 / scale.y), pos.z);
+        transform.localPosition = pos;
+    }
 
     void OnGUI()
     {
