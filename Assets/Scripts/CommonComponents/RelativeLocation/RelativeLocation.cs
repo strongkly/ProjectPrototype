99042333232486f16ca479c1d964976a56596fca
zhangqi <379497489@qq.com>
2018-10-26 18:19:58 +0800

    relative location will work properly without any pivot and anchor settings required now.

--- Assets/Scripts/CommonComponents/RelativeLocation/RelativeLocation.cs
+++ Assets/Scripts/CommonComponents/RelativeLocation/RelativeLocation.cs
@@ -38,44 +38,16 @@ namespace CrazyBox.Components
             }
         }
 
-        /// <summary>
-        /// Rely on parent width and height,be sure both parent of re-location and relatee RectTransform are 
-        /// set to the same size
-        /// </summary>
-        /// <param name="relatee"></param>
-        /// <param name="direction"></param>
-        /// <returns></returns>
-        public Vector3 FixAnchoredPosition(RectTransform relatee = null,
-            RelativeLocationDirection direction = RelativeLocationDirection.right)
-        {
-            this.relatee = relatee ?? this.relatee;
-            Vector3 result = this.relatee.anchoredPosition;
-            result = GetRelateeSourcePos(direction);
-            result = relateeParentTrans.TransformPoint(result);
-
-            result = transform.parent.InverseTransformPoint(result);
-            AdjustRelatorPos(ref result, direction);
-            result = selfRectrans.AdjustAnchoredPosition(result);
-            selfRectrans.anchoredPosition = result;
-
-            return result;
-        }
-
-        /// <summary>
-        /// Be sure both pivots of re-location and relatee RectTransform are set to the same value
-        /// </summary>
-        /// <param name="relatee"></param>
-        /// <param name="direction"></param>
-        /// <returns></returns>
         public Vector3 FixLocalPosition(RectTransform relatee = null,
             RelativeLocationDirection direction = RelativeLocationDirection.right)
         {
             this.relatee = relatee ?? this.relatee;
-            Vector3 result = this.relatee.localPosition;
-            result = GetRelateeLocalPos(direction);
+            Vector3 result = GetRelateeLocalPos(direction);
+            result = this.relatee.LocalPosToCenterPos(result);
             result = this.relatee.parent.TransformPoint(result);
 
             result = transform.parent.InverseTransformPoint(result);
+            result = selfRectrans.CenterPosToLocalPos(result);
             result = AdjustRelatorLocalPos(result, direction);
             selfRectrans.localPosition = result;
 
@@ -135,159 +107,6 @@ namespace CrazyBox.Components
             }
             return localPos + delta;
         }
-
-        Vector3 GetRelateeSourcePos(RelativeLocationDirection direction =
-            RelativeLocationDirection.right)
-        {
-            Vector3 result;
-            switch (direction)
-            {
-                case RelativeLocationDirection.left:
-                    result = GetLeftPosInFreeAnchor();
-                    break;
-                case RelativeLocationDirection.right:
-                    result = GetRightPosInFreeAnchor();
-                    break;
-                case RelativeLocationDirection.center:
-                    result = GetCenterPosInFreeAnchor();
-                    break;
-                case RelativeLocationDirection.top:
-                    result = GetTopPosInFreeAnchor();
-                    break;
-                case RelativeLocationDirection.bottom:
-                    result = GetBottomPosInFreeAnchor();
-                    break;
-                default:
-                    result = Vector3.zero;
-                    break;
-            }
-            return result;
-        }
-
-        void AdjustRelatorPos(ref Vector3 original, RelativeLocationDirection direction =
-            RelativeLocationDirection.right)
-        {
-            switch (direction)
-            {
-                case RelativeLocationDirection.left:
-                    AdjustLeftPos(ref original);
-                    break;
-                case RelativeLocationDirection.right:
-                    AdjustRightPos(ref original);
-                    break;
-                case RelativeLocationDirection.center:
-                    AdjustCenterPos(ref original);
-                    break;
-                case RelativeLocationDirection.top:
-                    AdjustTopPos(ref original);
-                    break;
-                case RelativeLocationDirection.bottom:
-                    AdjustBottomPos(ref original);
-                    break;
-                default:
-                    break;
-            }
-        }
-
-        #region relatee pos 
-        Vector3 GetRightPosInFreeAnchor()
-        {
-            Vector3 result = Vector3.zero;
-            result.x = relatee.anchorMin.x * GetRelateeParentWidth() +
-                relatee.offsetMin.x + GetRelateeWidth();
-            result.y = relatee.anchorMin.y * GetRelateeParentHeight() +
-                relatee.offsetMin.y + GetRelateeHeight() / 2;
-
-            return result;
-        }
-
-        Vector3 GetLeftPosInFreeAnchor()
-        {
-            Vector3 result = Vector3.zero;
-            result.x = relatee.anchorMin.x * GetRelateeParentWidth() +
-                relatee.offsetMin.x;
-            result.y = relatee.anchorMin.y * GetRelateeParentHeight() +
-                relatee.offsetMin.y + GetRelateeHeight() / 2;
-
-            return result;
-        }
-
-        Vector3 GetCenterPosInFreeAnchor()
-        {
-            Vector3 result = Vector3.zero;
-            result.x = relatee.anchorMin.x * GetRelateeParentWidth() +
-                relatee.offsetMin.x + GetRelateeWidth() / 2;
-            result.y = relatee.anchorMin.y * GetRelateeParentHeight() +
-                relatee.offsetMin.y + GetRelateeHeight() / 2;
-
-            return result;
-        }
-
-        Vector3 GetTopPosInFreeAnchor()
-        {
-            Vector3 result = Vector3.zero;
-            result.x = relatee.anchorMin.x * GetRelateeParentWidth() +
-                relatee.offsetMin.x + GetRelateeWidth() / 2;
-            result.y = relatee.anchorMin.y * GetRelateeParentHeight() +
-                relatee.offsetMin.y + GetRelateeHeight();
-
-            return result;
-        }
-
-        Vector3 GetBottomPosInFreeAnchor()
-        {
-            Vector3 result = Vector3.zero;
-            result.x = relatee.anchorMin.x * GetRelateeParentWidth() +
-                relatee.offsetMin.x + GetRelateeWidth() / 2;
-            result.y = relatee.anchorMin.y * GetRelateeParentHeight() +
-                relatee.offsetMin.y;
-
-            return result;
-        }
-        #endregion
-
-        #region relocation pos
-        void AdjustRightPos(ref Vector3 localPos)
-        {
-            Vector2 anchorCenter = selfRectrans.GetRelativeAnchor(); //GetAnchorCenter(selfRectrans);
-            localPos.x = localPos.x - anchorCenter.x *
-                GetParentWidth() + GetSelfWidth() / 2;
-            localPos.y = localPos.y - anchorCenter.y *
-                GetParentHeight();
-        }
-
-        void AdjustLeftPos(ref Vector3 localPos)
-        {
-            Vector2 anchorCenter = selfRectrans.GetRelativeAnchor();
-            localPos.x = localPos.x - anchorCenter.x *
-                GetParentWidth() - GetSelfWidth() / 2;
-            localPos.y = localPos.y - anchorCenter.y *
-                GetParentHeight();
-        }
-
-        void AdjustCenterPos(ref Vector3 localPos)
-        {
-            localPos = selfRectrans.InversedTransformPointToAnchoredPoint(localPos);
-        }
-
-        void AdjustTopPos(ref Vector3 localPos)
-        {
-            Vector2 anchorCenter = selfRectrans.GetRelativeAnchor();
-            localPos.x = localPos.x - anchorCenter.x *
-                GetParentWidth();
-            localPos.y = localPos.y - anchorCenter.y *
-                GetParentHeight() + GetSelfHeight() / 2;
-        }
-
-        void AdjustBottomPos(ref Vector3 localPos)
-        {
-            Vector2 anchorCenter = selfRectrans.GetRelativeAnchor();
-            localPos.x = localPos.x - anchorCenter.x *
-                GetParentWidth();
-            localPos.y = localPos.y - anchorCenter.y *
-                GetParentHeight() - GetSelfHeight() / 2;
-        }
-        #endregion
 
         #region Assist func
         float GetRelateeParentWidth()
