074d89b9eb883f80d71363963187d69a69d99e1e
zhangqi <379497489@qq.com>
2018-11-06 21:06:32 +0800

    add ramp alpha mask

---
+++ Assets/Test/TestScripts/TestAlphaMask.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAlphaMask : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+	    	
+	}
+
+
+    void OnGUI()
+    {
+        if (GUILayout.Button("update mask"))
+        {
+            gameObject.GetComponent<CrazyBox.Tools.Mask>().UpdateMask();
+        }
+    }
+}
---
+++ Assets/Test/TestScripts/TestAnchorPos.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class TestAnchorPos : MonoBehaviour
+{
+    void OnGUI()
+    {
+        GUILayout.Label((transform as RectTransform).localPosition.ToString());
+        GUILayout.Label(GetCenterPosition().ToString());
+    }
+
+    RectTransform selfRect
+    {
+        get
+        {
+            return transform as RectTransform;
+        }
+    }
+
+    Vector2 GetCenterPosition()
+    {
+        Vector2 pos = Vector2.zero;
+        pos.x = transform.localPosition.x;
+        pos.y = transform.localPosition.y;
+
+        pos = selfRect.LocalPosToCenterPos(pos);
+
+        return pos;
+    }
+}
