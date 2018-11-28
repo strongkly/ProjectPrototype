c328c64a91367398858c7c6c75bfbe4a5560f7f2
zhangqi <379497489@qq.com>
2018-11-28 19:47:40 +0800

    add multiple item list

--- Assets/Test/TestScripts/TestReusableItemList.cs
+++ Assets/Test/TestScripts/TestReusableItemList.cs
@@ -6,19 +6,29 @@ using UnityEngine.UI;
 
 public class TestReusableItemList : MonoBehaviour
 {
-    public RectTransform ItemListParent;
+    public RectTransform ItemListParent, mItemListParent;
 
     List<string> values = new List<string>() { "1", "2", "3" };
+    List<TestMultipleGroupedData> mValues = new List<TestMultipleGroupedData>() {
+        new TestMultipleGroupedData(0,"1"),
+        new TestMultipleGroupedData(1,"2"),
+        new TestMultipleGroupedData(1,"3")};
 
     ReusableItemList<TestReusableItem, string> itemList;
+
+    MultipleStyleReusableItemList<TestMultipleGroupedData> mItemList;
 
     private void Start()
     {
         itemList = new ReusableItemList<TestReusableItem, string>(ItemListParent);
+        mItemList = MultipleStyleReusableItemList<TestMultipleGroupedData>.
+            Create<TestMultipleReusableItem1, TestMultipleReusableItem2>(mItemListParent);
     }
 
     private void OnGUI()
     {
+        GUILayout.BeginVertical();
+        GUILayout.BeginHorizontal();
         if (GUILayout.Button("Clear"))
         {
             itemList.ReuseAll();
@@ -39,6 +49,31 @@ public class TestReusableItemList : MonoBehaviour
                 Debug.LogError("items less than 4");
             }
         }
+        GUILayout.EndHorizontal();
+
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button("mClear"))
+        {
+            mItemList.ReuseAll();
+        }
+        if (GUILayout.Button("mShow"))
+        {
+            mItemList.RefreshAll(mValues);
+        }
+        if (GUILayout.Button("mAdd"))
+        {
+            mValues.Add(new TestMultipleGroupedData(0, "123"));
+            mItemList.RefreshAll(mValues);
+        }
+        if (GUILayout.Button("mRefresh item4"))
+        {
+            if (!mItemList.TryRefreshByIdx(3))
+            {
+                Debug.LogError("items less than 4");
+            }
+        }
+        GUILayout.EndHorizontal();
+        GUILayout.EndVertical();
     }
 
     public class TestReusableItem : ReusableItem<string>
@@ -60,6 +95,64 @@ public class TestReusableItemList : MonoBehaviour
         {
             base.Refresh();
             Debug.LogError("refreshing");
+        }
+    }
+
+    public class TestMultipleReusableItem1 : GroupedReusableItem<TestMultipleGroupedData>
+    {
+        Text text;
+
+        private void Awake()
+        {
+            text = transform.Find("Text").GetComponent<Text>();
+        }
+
+        public override void Refresh(TestMultipleGroupedData data, int dataIndex)
+        {
+            base.Refresh(data, dataIndex);
+            text.text = data.text;
+        }
+
+        public override void Refresh()
+        {
+            base.Refresh();
+            Debug.LogError("refreshing");
+        }
+    }
+    public class TestMultipleReusableItem2 : GroupedReusableItem<TestMultipleGroupedData>
+    {
+        Text text;
+
+        private void Awake()
+        {
+            text = transform.Find("Text").GetComponent<Text>();
+        }
+
+        public override void Refresh(TestMultipleGroupedData data, int dataIndex)
+        {
+            base.Refresh(data, dataIndex);
+            text.text = data.text;
+        }
+
+        public override void Refresh()
+        {
+            base.Refresh();
+            Debug.LogError("refreshing");
+        }
+    }
+
+    public class TestMultipleGroupedData : IGroupedData
+    {
+        public int idx;
+        public string text;
+        public TestMultipleGroupedData(int idx, string text)
+        {
+            this.idx = idx;
+            this.text = text;
+        }
+        public int GetGroupId()
+        {
+            return idx;
         }
     }
 }
