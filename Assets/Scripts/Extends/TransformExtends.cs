1c800e067789c4ed697cd6a6e9cd18a32ac0e19b
zhangqi <379497489@qq.com>
2018-11-28 12:48:20 +0800

    1.add extend class for transform 2.add testing scene for reusableItemList

---
+++ Assets/Scripts/Extends/TransformExtends.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public static class TransformExtends
+{
+    public static bool TryFindTransformFromDescendantsByName(this Transform parent, 
+        ref TransformExtends findTrans, string name)
+    {
+        bool result = false;
+
+        if (parent.childCount != 0)
+        {
+            for (int i = 0; i < parent.childCount; i++)
+            {
+                if (parent.GetChild(i).name == name)
+                {
+                    findTrans = parent.GetChild(i);
+                    result = true;
+                    break;
+                }
+            }
+            if (!result)
+            {
+                for (int i = 0; i < parent.childCount; i++)
+                {
+                    if (TryFindTransformInChildByName(parent.GetChild(i), ref findTrans, name))
+                    {
+                        result = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+}
