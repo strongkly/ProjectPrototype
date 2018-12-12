a120838cc4bb8186fd0f655ef1397c7477c0db45
zhangqi <379497489@qq.com>
2018-12-12 14:16:38 +0800

    combine test scripts for relativefix

--- Assets/Test/TestScripts/TestRelativeFix.cs
+++ Assets/Test/TestScripts/TestRelativeFix.cs
@@ -1,30 +1,51 @@
 ﻿using UnityEngine;
-using UnityEngine.EventSystems;
-using System;
+using UnityEngine.UI;
 using CrazyBox.Components;
+using UnityEngine.EventSystems;
 
 public class TestRelativeFix : MonoBehaviour {
 
+    public Image img;
+    public GameObject relocatingObj;
+    RelativeFix fix;
+
     void Start()
     {
-        DragHandler.Get(gameObject).OnDragAction = OnDrag;
+        DragHandler.Get(relocatingObj).OnDragAction = OnDrag;
+
+        #region rejectfix
+        fix = RelativeFix.Get(img.transform);
+        DragHandler dh = DragHandler.Get(img.gameObject);
+        dh.OnEndAction = (ped) =>
+        {
+            fix.RejectFixPos(img.transform.parent as RectTransform);
+        };
+        dh.OnDragAction = (ped) =>
+        {
+            //mapContainer.transform.localPosition += ped.delta
+            Vector2 pos = img.transform.localPosition;
+            pos += ped.delta;
+            img.transform.localPosition = pos;
+        };
+        #endregion
     }
 
     public void OnDrag(PointerEventData ped)
     {
-        Vector3 pos = transform.localPosition;
-        Vector3 scale = transform.lossyScale;
+        Vector3 pos = relocatingObj.transform.localPosition;
+        Vector3 scale = relocatingObj.transform.lossyScale;
         pos.Set(pos.x + ped.delta.x * (1 / scale.x),
             pos.y + ped.delta.y * (1 / scale.y), pos.z);
-        transform.localPosition = pos;
+        relocatingObj.transform.localPosition = pos;
     }
 
     void OnGUI()
     {
-        GUILayout.Label((transform as RectTransform).anchoredPosition.ToString());
+        GUILayout.Label((relocatingObj.transform as RectTransform).
+            anchoredPosition.ToString());
         if (GUILayout.Button("调整位置"))
         {
-            GetComponent<RelativeFix>().LimitFixPos();
+            relocatingObj.GetComponent<RelativeFix>().LimitFixPos();
         }
     }
 }
