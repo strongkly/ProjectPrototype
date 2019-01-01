7f3056f540175d2b9eb84f9d7ed081fd5fa84768
zhangqi <379497489@qq.com>
2019-01-01 18:34:55 +0800

    change FingerGesture system to scriptsingleton

--- Assets/Scripts/Systems/GestureSystem/FingerGesture.cs
+++ Assets/Scripts/Systems/GestureSystem/FingerGesture.cs
@@ -1,118 +1,122 @@
 ﻿using UnityEngine;
 using UnityEngine.Events;
+using CrazyBox.Tools;
 
-public class FingerGesture : MonoBehaviour
+namespace CrazyBox.Systems
 {
-    private Vector2 prevDist = new Vector2(0, 0);
-    private Vector2 curDist = new Vector2(0, 0);
-
-    FingerGestrueEvent<float> OnZoomIn, OnZoomOut;
-    FingerGestrueEvent OnGestureEnd;
-
-    bool isOn = false;
-    public bool IsOn
+    public class FingerGesture : ScriptSingleton<FingerGesture>
     {
-        set
+        private Vector2 prevDist = new Vector2(0, 0);
+        private Vector2 curDist = new Vector2(0, 0);
+
+        FingerGestrueEvent<float> OnZoomIn, OnZoomOut;
+        FingerGestrueEvent OnGestureEnd;
+
+        bool isOn = false;
+        public bool IsOn
         {
-            isOn = value;
-            if (!isOn)
+            set
             {
-                ClearAllEventListeners();
+                isOn = value;
+                if (!isOn)
+                {
+                    ClearAllEventListeners();
+                }
+            }
+            get
+            {
+                return isOn;
             }
         }
-        get
+
+        private void Awake()
         {
-            return isOn;
+            OnZoomIn = new FingerGestrueEvent<float>();
+            OnZoomOut = new FingerGestrueEvent<float>();
+            OnGestureEnd = new FingerGestrueEvent();
         }
-    }
 
-    private void Awake()
-    {
-        OnZoomIn = new FingerGestrueEvent<float>();
-        OnZoomOut = new FingerGestrueEvent<float>();
-        OnGestureEnd = new FingerGestrueEvent();
-    }
-
-    void Update()
-    {
-        if (isOn)
-            CheckForMultiTouch();
-    }
-
-    void CheckForMultiTouch()
-    {
-        if (Input.touchCount == 2 && Input.GetTouch(0).phase == TouchPhase.Moved &&
-            Input.GetTouch(1).phase == TouchPhase.Moved)
+        void Update()
         {
-            curDist = Input.GetTouch(0).position - Input.GetTouch(1).position; 
-            prevDist = ((Input.GetTouch(0).position - Input.GetTouch(0).deltaPosition) -
-                (Input.GetTouch(1).position - Input.GetTouch(1).deltaPosition)); 
-            float touchDelta = curDist.magnitude - prevDist.magnitude;
-            if (touchDelta > 0)
+            if (isOn)
+                CheckForMultiTouch();
+        }
+
+        void CheckForMultiTouch()
+        {
+            if (Input.touchCount == 2 && Input.GetTouch(0).phase == TouchPhase.Moved &&
+                Input.GetTouch(1).phase == TouchPhase.Moved)
             {
-                if (OnZoomOut != null) OnZoomOut.Invoke(touchDelta);
+                curDist = Input.GetTouch(0).position - Input.GetTouch(1).position;
+                prevDist = ((Input.GetTouch(0).position - Input.GetTouch(0).deltaPosition) -
+                    (Input.GetTouch(1).position - Input.GetTouch(1).deltaPosition));
+                float touchDelta = curDist.magnitude - prevDist.magnitude;
+                if (touchDelta > 0)
+                {
+                    if (OnZoomOut != null) OnZoomOut.Invoke(touchDelta);
+                }
+                else if (touchDelta < 0)
+                {
+                    if (OnZoomIn != null) OnZoomIn.Invoke(touchDelta);
+                }
             }
-            else if (touchDelta < 0)
+            else if (Input.touchCount == 2 &&
+                (Input.GetTouch(0).phase == TouchPhase.Ended ||
+                Input.GetTouch(0).phase == TouchPhase.Canceled ||
+                Input.GetTouch(1).phase == TouchPhase.Ended ||
+                Input.GetTouch(1).phase == TouchPhase.Canceled))
             {
-                if (OnZoomIn != null) OnZoomIn.Invoke(touchDelta);
+                if (OnGestureEnd != null) OnGestureEnd.Invoke();
             }
         }
-        else if (Input.touchCount == 2 &&
-            (Input.GetTouch(0).phase == TouchPhase.Ended ||
-            Input.GetTouch(0).phase == TouchPhase.Canceled ||
-            Input.GetTouch(1).phase == TouchPhase.Ended ||
-            Input.GetTouch(1).phase == TouchPhase.Canceled))
+
+        public void ClearAllEventListeners()
         {
-            if (OnGestureEnd != null) OnGestureEnd.Invoke();
+            OnZoomIn.RemoveAllListeners();
+            OnZoomOut.RemoveAllListeners();
+            OnGestureEnd.RemoveAllListeners();
         }
+
+        #region API
+        public void SetZoomInCallBack(UnityAction<float> OnZoomIn)
+        {
+            if (OnZoomIn != null) this.OnZoomIn.AddListener(OnZoomIn);
+        }
+
+        public void RemoveZoomInCallBack(UnityAction<float> OnZoomIn)
+        {
+            if (OnZoomIn != null) this.OnZoomIn.RemoveListener(OnZoomIn);
+        }
+
+        public void SetZoomOutCallBack(UnityAction<float> OnZoomOut)
+        {
+            if (OnZoomOut != null) this.OnZoomOut.AddListener(OnZoomOut);
+        }
+
+        public void RemoveZoomOutCallBack(UnityAction<float> OnZoomOut)
+        {
+            if (OnZoomOut != null) this.OnZoomOut.RemoveListener(OnZoomOut);
+        }
+
+        public void SetGestureEndCallBack(UnityAction OnGestureEnd)
+        {
+            if (OnGestureEnd != null) this.OnGestureEnd.AddListener(OnGestureEnd);
+        }
+
+        public void RemoveGestureEndCallBack(UnityAction OnGestureEnd)
+        {
+            if (OnGestureEnd != null) this.OnGestureEnd.RemoveListener(OnGestureEnd);
+        }
+        #endregion
     }
 
-    public void ClearAllEventListeners()
+    public class FingerGestrueEvent : UnityEvent
     {
-        OnZoomIn.RemoveAllListeners();
-        OnZoomOut.RemoveAllListeners();
-        OnGestureEnd.RemoveAllListeners();
+
     }
 
-    #region API
-    public void SetZoomInCallBack(UnityAction<float> OnZoomIn)
+    public class FingerGestrueEvent<T> : UnityEvent<T>
     {
-        if (OnZoomIn != null) this.OnZoomIn.AddListener(OnZoomIn);
+
     }
-
-    public void RemoveZoomInCallBack(UnityAction<float> OnZoomIn)
-    {
-        if(OnZoomIn != null) this.OnZoomIn.RemoveListener(OnZoomIn);
-    }
-
-    public void SetZoomOutCallBack(UnityAction<float> OnZoomOut)
-    {
-        if (OnZoomOut != null) this.OnZoomOut.AddListener(OnZoomOut);
-    }
-
-    public void RemoveZoomOutCallBack(UnityAction<float> OnZoomOut)
-    {
-        if (OnZoomOut != null) this.OnZoomOut.RemoveListener(OnZoomOut);
-    }
-
-    public void SetGestureEndCallBack(UnityAction OnGestureEnd)
-    {
-        if (OnGestureEnd != null) this.OnGestureEnd.AddListener(OnGestureEnd);
-    }
-
-    public void RemoveGestureEndCallBack(UnityAction OnGestureEnd)
-    {
-        if (OnGestureEnd != null) this.OnGestureEnd.RemoveListener(OnGestureEnd);
-    }
-    #endregion
-}
-
-public class FingerGestrueEvent : UnityEvent
-{
-    
-}
-
-public class FingerGestrueEvent<T> : UnityEvent<T>
-{
-    
 }
