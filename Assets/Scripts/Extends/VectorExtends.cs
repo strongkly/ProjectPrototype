4326a17d51f279818621b1158ab6818d4ee0f314
zhangqi <379497489@qq.com>
2019-01-17 11:45:05 +0800

    Add VectorExtends

---
+++ Assets/Scripts/Extends/VectorExtends.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public static class VectorExtends
+{
+    /// <summary>
+    /// Rotate this point around pivot with eulerAngles
+    /// <para> use <see cref="Transform.RotateAround"/> when tring to rotate a GameObject</para>
+    /// </summary>
+    public static Vector3 RotateAroundPivot(this Vector3 self, Vector3 pivot, float x, float y, float z)
+    {
+        Vector3 dir = self - pivot;
+        dir = Quaternion.Euler(x, y, z) * dir;
+        self = dir + pivot;
+        return self;
+    }
+
+    /// <summary>
+    /// Rotate this point around pivot with eulerAngles
+    /// <para> use <see cref="Transform.RotateAround"/> when tring to rotate a GameObject</para>
+    /// </summary>
+    public static Vector3 RotateAroundPivot(this Vector3 self, Vector3 pivot, Vector3 eulerAngles)
+    {
+        Vector3 dir = self - pivot;
+        dir = Quaternion.Euler(eulerAngles) * dir;
+        self = dir + pivot;
+        return self;
+    }
+}
