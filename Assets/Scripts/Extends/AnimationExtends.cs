5df9702da6b4cf8112d125a9b0dbd880f9760987
zhangqi <379497489@qq.com>
2019-02-20 12:08:39 +0800

    add animation extend

---
+++ Assets/Scripts/Extends/AnimationExtends.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public static class AnimationExtends
+{
+    public static void RewindToFirstFrame(this Animation animation)
+    {
+        if (animation.isPlaying)
+        {
+            animation.Rewind();
+        }
+        else
+        {
+            animation.Play();
+            animation.Sample();
+            animation.Stop();
+        }
+    }
+}
